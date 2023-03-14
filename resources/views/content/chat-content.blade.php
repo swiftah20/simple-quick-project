03dad451bd68125b0b754df2b167eac1400d1348
swiftah20 <sie.miftah@gmail.com>
2023-03-15 00:07:25 +0700

    inbox chat done

---
+++ resources/views/content/chat-content.blade.php
@@ -0,0 +1,26 @@
+<div class="modal-header">
+    <div class="row">
+        <div class="col-sm-1">
+            <button type="button" class="btn w-100 h-100">
+                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M16 7H3.83L9.42 1.41L8 0L0 8L8 16L9.41 14.59L3.83 9H16V7Z" fill="#333333" />
+                </svg>
+            </button>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm">
+            <div class="chat-title">
+                I-589 - AMARKHIL, Obaidullah [Affirmative Filing with ZHN]
+            </div>
+        </div>
+    </div>
+    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
+<div class="modal-container mt-3">
+    <div class="row">
+        1
+    </div>
+</div>
